74123cee01566070879afefe8a9b1ce77e415a09
Y4sh-4 <yeziieditz@gmail.com>
2026-09-24 23:59:57 +0530

    Top Jobs skills

--- Project Sql/Skills for Top Jobs.sql
+++ Project Sql/Skills for Top Jobs.sql
@@ -6,30 +6,24 @@ SELECT
     salary_year_avg,
    COUNT (skills_dim.skills) AS skill_count,
     job_schedule_type,
-    job_location,
-    job_country,
-    job_posted_date,
    STRING_AGG ( skills_dim.skills, ',') AS job_skill
  
 
 FROM 
     job_postings_fact 
 LEFT JOIN  company_dim ON  job_postings_fact.company_id = company_dim.company_id
-LEFT JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-LEFT JOIN skills_dim ON  skills_job_dim.skill_id = skills_dim.skill_id 
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON  skills_job_dim.skill_id = skills_dim.skill_id 
 
 WHERE 
     job_title_short = 'Software Engineer'
 AND salary_year_avg IS NOT NULL AND job_location = 'Anywhere'
    
 GROUP BY 
-     company_name, 
+    company_name, 
     job_title_short,
     salary_year_avg,
-    job_schedule_type,
-    job_location,
-    job_country,
-    job_posted_date
+    job_schedule_type
  
 ORDER BY 
     salary_year_avg DESC
--- Project Sql/Top_paying_job.sql
+++ Project Sql/Top_paying_job.sql
@@ -1,16 +1,18 @@
 
 SELECT 
     job_id,
+    Name AS company_name, 
     job_title_short,
-    job_schedule_type,
     salary_year_avg,
-    job_country,
+    job_schedule_type,
     job_location,
+    job_country,
     job_posted_date
  
 
 FROM 
     job_postings_fact 
+LEFT JOIN  company_dim ON  job_postings_fact.company_id = company_dim.company_id
 WHERE job_title_short = 'Software Engineer'
 AND salary_year_avg IS NOT NULL AND job_location = 'Anywhere'
     
